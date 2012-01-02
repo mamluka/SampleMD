72754347e039bff90f560f27e275d421987afc4d
David <david.mazvovsky@gmail.com>
2012-01-02 16:26:43 +0200

    Added reading of configurations from XML

--- Configurations/class_ConfigurationManager.f90
+++ Configurations/class_ConfigurationManager.f90
@@ -1,5 +1,6 @@
 module class_ConfigurationManager
     use flib_dom
+    use class_ErrorHandler
     implicit none
 
 
@@ -14,7 +15,7 @@ contains
 
     function LoadSimulationConfigurations(filename) result (configurations)
         type (SimulationConfigurations) :: configurations
-        character (len=200) :: filename
+        character (len=*) :: filename
 
         type(fnode), pointer          :: confDoc
         type(fnode), pointer          :: rootNode,simulationNode
@@ -23,43 +24,60 @@ contains
 
         integer :: i
 
-        confDoc => parsefile("c:\mdconfig.xml")
-        !confDoc => parsefile(filename)
+
+        confDoc => parsefile(filename)
+
+        call normalize(confDoc)
 
 
         tmpList => getElementsByTagName(confDoc, "mdconfig")
 
         rootNode => item(tmpList, 0)
 
-        configNodes => getChildNodes(rootNode)
+        tmpList => getElementsByTagName(rootNode, "sim")
 
-        do i = 0,getLength(configNodes)
-            simulationNode => item(configNodes,i)
-
-            simulatiomNodeName=char(getNodeName(simulationNode))
-            simulatiomNodeValue=char(getNodeValue(simulationNode))
+        if (getLength(tmpList) == 0) then
+            call die("When asked to load the simulation configuration the xml element was not found")
+        endif
 
 
-            print *,simulatiomNodeValue,simulatiomNodeName
+        simulationNode => item(tmpList, 0)
 
-            select case (simulatiomNodeName)
-                case ("dim")
-                    read(unit=simulatiomNodeValue,fmt=*) configurations%Dimension
-                case ("timestep")
-                    read(unit=simulatiomNodeValue,fmt=*) configurations%TimeStep
-                case default
-
-            end select
+        configurations%Dimension = LoadXMLAttributeToInt(simulationNode,"dim")
+        configurations%TimeStep = LoadXMLAttributeToReal(simulationNode,"timestep")
 
 
-        enddo
+        !simAttribute = getAttribute(myNode,"dim")
 
-
-
-
-
-
+       ! if (simAttribute == "") call die("When asked to load the dim from xml the attribute couldn't be found")
+       ! read(unit=simAttribute,fmt=*) configurations%Dimension
 
     end function
+
+    function LoadXMLAttributeToReal(node,nameOfAttr) result (returnValue)
+        type(fnode) , pointer :: node
+        character (len=*) :: nameOfAttr
+        real :: returnValue
+        character (len=100) :: simAttribute
+
+        simAttribute = getAttribute(node,nameOfAttr)
+
+        if (simAttribute == "") call die("When asked to load " // nameOfAttr // " from xml the attribute couldn't be found")
+        read(unit=simAttribute,fmt=*) returnValue
+
+    end function LoadXMLAttributeToReal
+
+    function LoadXMLAttributeToInt(node,nameOfAttr) result (returnValue)
+        type(fnode) , pointer :: node
+        character (len=*) :: nameOfAttr
+        integer :: returnValue
+        character (len=100) :: simAttribute
+
+        simAttribute = getAttribute(node,nameOfAttr)
+
+        if (simAttribute == "") call die("When asked to load " // nameOfAttr // " from xml the attribute couldn't be found")
+        read(unit=simAttribute,fmt=*) returnValue
+
+    end function LoadXMLAttributeToInt
 
 end module class_ConfigurationManager
